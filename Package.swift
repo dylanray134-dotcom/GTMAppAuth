c80cdfb57dbcd18ad1124d242ea83fb7b0a19618
Dylan Womack <dylanray134@gmail.com>
2026-09-11 01:40:53 +0000

    Point SPM deps at dylanray134-dotcom forks for Xcode Cloud

--- Package.swift
+++ Package.swift
@@ -32,8 +32,8 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/google/gtm-session-fetcher.git", "3.3.0" ..< "6.0.0"),
-    .package(url: "https://github.com/openid/AppAuth-iOS.git", "3.0.0" ..< "4.0.0")
+    .package(url: "https://github.com/dylanray134-dotcom/gtm-session-fetcher.git", "3.3.0" ..< "6.0.0"),
+    .package(url: "https://github.com/dylanray134-dotcom/AppAuth-iOS.git", "3.0.0" ..< "4.0.0")
   ],
   targets: [
     .target(
